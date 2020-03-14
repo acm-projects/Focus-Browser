b528d71de97357e781f1425a8d9cc5c0f6681abc
Yuvraj Thakur <Yuvrajt463@gmail.com>
2020-03-13 21:46:06 -0500

    Changed Basic Code from a VBox to a BorderPane layout with multiple tempoary nodes for url bar and notes tab

--- FocusBrowser/src/focusbrowser/Main.java
+++ FocusBrowser/src/focusbrowser/Main.java
@@ -1,9 +1,16 @@
 package focusbrowser;
 
+import java.awt.Button;
+import java.awt.Label;
+
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
@@ -12,12 +19,35 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-    	
-    	WebView window = new WebView();
-    	WebEngine url = window.getEngine();
-    	url.load("https://www.google.com");
-        Scene scene = new Scene(window,750,500);
         
+    	BorderPane bPane = new BorderPane();
+        
+    	WebView tab = new WebView();
+    	WebEngine url = tab.getEngine();
+    	url.load("https://www.google.com");
+    	
+    	
+    	
+    	//Placeholder for Tabs and URL
+    	Text tabAndUrl = new Text("Tabs and Url bar here");
+    	
+    	tabAndUrl.setFont(new Font(30.0));
+    	tabAndUrl.setTextAlignment(TextAlignment.CENTER);
+    	//
+    	
+    	
+    	//Placeholder for Notes
+    	Text notes = new Text("NOTES TAB");
+    	
+    	notes.setFont(new Font(30.0));
+    	notes.setTextAlignment(TextAlignment.CENTER);
+    	//
+    	
+    	bPane.setTop(tabAndUrl);
+    	bPane.setLeft(notes);
+        bPane.setCenter(tab);
+        
+        Scene scene = new Scene(bPane,750,500);
         
         primaryStage.setScene(scene);
         primaryStage.setMaximized(true);
