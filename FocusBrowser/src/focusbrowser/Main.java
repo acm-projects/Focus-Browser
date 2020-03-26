9dc5ef2a4c09b40fdbebb7f92fcc9d2ee1e0faaa
Stuart Little <stuartlittle6@yahoo.com>
2020-03-25 20:51:49 -0500

    Added webview connection. Textfield needs to be added to work.

--- FocusBrowser/src/focusbrowser/Main.java
+++ FocusBrowser/src/focusbrowser/Main.java
@@ -4,8 +4,11 @@ import java.awt.Button;
 import java.awt.Label;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
@@ -15,33 +18,38 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application
+{
+	
+	// Global Variables
+	WebView tab = new WebView();
+	WebEngine url = tab.getEngine();
+	TextField textField = new TextField();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         
-    	BorderPane bPane = new BorderPane();
+        textField.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+    	    public void handle(ActionEvent event) {
+    	            url.load(textField.getText());
+    	    }
+    	});
         
-    	WebView tab = new WebView();
-    	WebEngine url = tab.getEngine();
+    	BorderPane bPane = new BorderPane();
+    	
     	url.load("https://www.google.com");
-    	
-    	
     	
     	//Placeholder for Tabs and URL
     	Text tabAndUrl = new Text("Tabs and Url bar here");
     	
     	tabAndUrl.setFont(new Font(30.0));
     	tabAndUrl.setTextAlignment(TextAlignment.CENTER);
-    	//
     	
-    	
-    	//Placeholder for Notes
+    	// Placeholder for Notes
     	Text notes = new Text("NOTES TAB");
     	
     	notes.setFont(new Font(30.0));
     	notes.setTextAlignment(TextAlignment.CENTER);
-    	//
     	
     	bPane.setTop(tabAndUrl);
     	bPane.setLeft(notes);
@@ -56,7 +64,8 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
 } 	
